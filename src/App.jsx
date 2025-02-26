035acad1ff858990f75671ccd133d7c792802a04
İlyas Torun <ilyastorunn@outlook.com>
2025-02-26 03:03:50 +0300

    habits tracker

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <Button>habits deneme</Button>      
+    </>
+  )
+}
+
+export default App
